d3f9d6f6f1ae9fa2386a2594c80072448bda7f24
georgescutelnicu <george.v.scutelnicu@gmail.com>
2023-11-04 01:34:24 +0200

    Add argparse for command-line argument parsing

--- train.py
+++ train.py
@@ -1,48 +1,70 @@
-
 import os
 import torch
 from pathlib import Path
-import matplotlib.pyplot as plt
-import data_setup, engine, model_builder, utils
+import argparse
+import data_setup
+import engine
+import model_builder
+import utils
 
-NUM_EPOCHS = 1
-NUM_WORKERS = os.cpu_count()
-BATCH_SIZE = 32
-LEARNING_RATE = 3e-4
 
-# Directories
-train_dir = Path('/content/train') 
-test_dir = Path('/content/test')
+def main(parsed_args):
+    num_epochs = parsed_args.epochs
+    num_workers = os.cpu_count()
+    batch_size = parsed_args.batch_size
+    learning_rate = parsed_args.learning_rate
 
-NUM_CLASSES = len(next(os.walk(train_dir))[1])
+    # Directories
+    train_dir = Path(parsed_args.train_dir)
+    test_dir = Path(parsed_args.test_dir)
 
-# Device agnostic code
-device = "cuda" if torch.cuda.is_available() else "cpu"
+    num_classes = len(next(os.walk(train_dir))[1])
 
-# Model
-model, transform = model_builder.create_effnetb2_model(num_classes=NUM_CLASSES)
-model.to(device)
+    # Device agnostic code
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Dataloaders
-train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(train_dir=train_dir, 
-                                                                               test_dir=test_dir, 
-                                                                               transform=transform, 
-                                                                               batch_size=BATCH_SIZE, 
-                                                                               num_workers=NUM_WORKERS)
+    # Model
+    model, transform = model_builder.create_effnetb2_model(num_classes=num_classes)
+    model.to(device)
 
-# Train model
-loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(),
-                             lr=LEARNING_RATE)
+    # Dataloaders
+    train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+        train_dir=train_dir,
+        test_dir=test_dir,
+        transform=transform,
+        batch_size=batch_size,
+        num_workers=num_workers
+    )
 
-results = engine.train(model=model,
-                       train_dataloader=train_dataloader,
-                       test_dataloader=test_dataloader,
-                       loss_fn=loss_fn,
-                       optimizer=optimizer,
-                       epochs=NUM_EPOCHS,
-                       device=device)
-                       
-# Save model
-utils.save_model(model=model,
-                 model_name="model_0.pth")
+    # Train model
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    results = engine.train(
+        model=model,
+        train_dataloader=train_dataloader,
+        test_dataloader=test_dataloader,
+        loss_fn=loss_fn,
+        optimizer=optimizer,
+        epochs=num_epochs,
+        device=device
+    )
+
+    # Save model and loss curve plot
+    loss_fig = utils.plot_loss_curve(results)
+    loss_fig.savefig("loss_curve.png")
+    utils.save_model(model=model, model_name=parsed_args.model_name)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Image Classification Training Script")
+
+    parser.add_argument('--train-dir', '-tr', type=str, required=True, help='Path to the training directory')
+    parser.add_argument('--test-dir', '-te', type=str, required=True, help='Path to the testing directory')
+    parser.add_argument('--epochs', '-e', type=int, required=True, help='Number of training epochs')
+    parser.add_argument('--batch_size', '-b', type=int, required=True, help='Batch size')
+    parser.add_argument('--learning_rate', '-lr', type=float, required=True, help='Learning rate')
+    parser.add_argument('--model_name', '-m', type=str, required=True, help='Name for the saved model file')
+
+    args = parser.parse_args()
+    main(args)
